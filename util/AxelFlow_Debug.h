d76f2389790ccab33e431956ea39e2904ebc5b0b
SrikarBharadwajR <srikarbharadwaj000@gmail.com>
2025-03-13 20:34:14 +0530

    dir restructured

--- util/AxelFlow_Debug.h
+++ util/AxelFlow_Debug.h
@@ -1,8 +1,9 @@
 #ifndef INC_AXELFLOW_DEBUG_H_
 #define INC_AXELFLOW_DEBUG_H_
 
-#include "AxelFlow.h"
+//#include "AxelFlow.h"
 #include "stm32f0xx_hal.h"
+#include <string.h>
 
 void AxelFlow_debug_init(UART_HandleTypeDef *huart);
 HAL_StatusTypeDef AxelFlow_debug_print(char* str);
